728be089242abae4d49bf93c96b42da77272f548
stradichenko <sittoche@gmail.com>
2026-03-27 16:20:06 +0100

    bump version to 0.1.5.12

--- _meta.lua
+++ _meta.lua
@@ -8,7 +8,7 @@ local _ = require("gettext")
 
 return {
     name = "audiobook",
-    version = "0.1.5.11",
+    version = "0.1.5.12",
     fullname = _("Audiobook Read-Along"),
     description = _([[Text-to-Speech with synchronized word highlighting.
 
